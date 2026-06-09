535a525f82ead0feb4bf33eec4f9ca8b724f8a05
mhusainh <husainhasbulah@gmail.com>
2026-06-09 15:44:36 +0700

    change max_pos_w safety limits

--- esp32s3_slave2_manipulator2/armbox_config.h
+++ esp32s3_slave2_manipulator2/armbox_config.h
@@ -98,7 +98,7 @@ const long MAX_ENCODER_POSITION = 2000; // fallback, dipakai per-axis di bawah
 const int MOVE_SPEED = 400;
 
 // Safety limits per-axis (max encoder count, hardware limit)
-const long MAX_POS_W =  500;  // Sumbu W (Rotasi) — range terbatas
+const long MAX_POS_W =  4000;  // Sumbu W (Rotasi) — range terbatas
 const long MAX_POS_Y = 2900;  // Sumbu Y (Maju Mundur)
 const long MAX_POS_Z = 4000; 
 
